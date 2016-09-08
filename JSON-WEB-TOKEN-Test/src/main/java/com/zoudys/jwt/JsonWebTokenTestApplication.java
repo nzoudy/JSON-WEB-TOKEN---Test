8840f78bbc1b94d8ea2e84ee2077bf8535a9f318
nzouda urbain <nzoudy@hotmail.fr>
2016-09-09 00:39:14 +0200

    first commit - just create project to test jwt

---
+++ JSON-WEB-TOKEN-Test/src/main/java/com/zoudys/jwt/JsonWebTokenTestApplication.java
@@ -0,0 +1,12 @@
+package com.zoudys.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonWebTokenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonWebTokenTestApplication.class, args);
+	}
+}
